9049c6cedce0d02a015d82a37443c01e1f2dfb04
c-woodman <cwoodma1@byu.edu>
2024-01-16 19:21:36 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using MoreCSharpFun;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        PrintStuff ps = new PrintStuff("EN");
+        
+        string name = "";
+
+        System.Console.WriteLine("Please enter your name");
+        name = System.Console.ReadLine();
+
+        ps.PrintName(name);
+
+    }
+
+}
